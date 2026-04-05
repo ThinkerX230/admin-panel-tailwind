154ac68de88591df0661c6f985ec3b73e2ce21ba
Mikaylazz <mikaylaarzetha@gmail.com>
2026-04-05 20:58:17 +0700

    commit terbaru components tableblade

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,23 @@
+<div class="mt-6 bg-white p-6 rounded-xl shadow">
+
+  <div class="flex justify-between mb-4">
+    <h3 class="font-semibold">Data Users</h3>
+    <button onclick="alert('Tambah Data')" 
+      class="bg-blue-500 text-white px-4 py-2 rounded">
+      + Tambah
+    </button>
+  </div>
+
+  <table class="w-full">
+    <tr class="bg-gray-100">
+      <th class="p-2">Nama</th>
+      <th class="p-2">Email</th>
+    </tr>
+
+    <tr>
+      <td class="p-2">Rafi</td>
+      <td class="p-2">rafi@mail.com</td>
+    </tr>
+  </table>
+
+</div>
